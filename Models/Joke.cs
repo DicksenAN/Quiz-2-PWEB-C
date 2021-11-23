dc68ea383042a3fea9dc0864acdde31c79c32459
DicksenAN <dicksenan@gmail.com>
2021-11-24 02:46:47 +0700

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace WebProCQuiz2.Models
+{
+    public class Joke
+    {
+        /// Joke's Properties
+        public int Id { get; set; }
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+
+        // Constructor
+        public Joke()
+        {
+
+        }
+
+    }
+}
